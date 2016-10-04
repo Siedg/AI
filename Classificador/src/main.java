4fb2901dc8c9922e17aa928f06e24c54638d03fc
Siedg <GuI05101618>
2016-10-04 08:06:17 -0300

    Classificador first commit.

---
+++ Classificador/src/main.java
@@ -0,0 +1,36 @@
+/**
+ * main.java
+ *
+ * @author <a href="https://www.github.com/Siedg">Siedg</a> (Guilherme Nascimento de Lima)
+ * @contact siedg104@gmail.com
+ * @date 04/10/2016 - 08:02
+ * @since GAMMA - 0x3
+ */
+public class main {
+  public static void main(String[] args) {
+    /**
+     *
+     *
+     *
+     *
+        Exemplo Código k-NN ;
+        % class = knnclassify(dadosTeste, dadosTreino, labelTeste) ;
+        % matConf = confusionmat(labelTeste,class) ;
+        % acurracy = sum(diag(matConf))/sum(matConf(:))
+
+        Exemplo Código AG’s ;
+        % gatool  %(modo terminal ga)
+        % fitness Function = @seuPrograma
+        % Number of Variables = 2048 (features)
+        % Population Type = Bit String
+
+        % O restante dos parâmetros faz parte do trabalho.
+     *
+     *
+     *
+     *
+     **/
+
+    
+  }
+}
